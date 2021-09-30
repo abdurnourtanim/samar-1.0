5af70f71d0933f500e2523d316b6b35a83f231dc
antanim <abdurnouutanim11@gmail.com>
2021-09-30 22:41:31 +0600

    hero section to protfolio section responsive done

---
+++ script/app.js
@@ -0,0 +1,52 @@
+// Mobile menu
+const mobileBar = document.querySelector(".mobile__bar");
+const mobileMenu = document.querySelector(".nav__items");
+mobileBar.addEventListener("click", () => {
+  mobileMenu.classList.toggle("nav__items__show");
+});
+
+// counter
+$(".count__number").counterUp({
+  delay: 10,
+  time: 2000,
+});
+
+// sponsor slick slider
+$(".slide__wrapper").slick({
+  autoplay: true,
+  slidesToShow: 6,
+  autoplaySpeed: 3000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+// disable all button click on reload page
+const form = document.querySelector("form");
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+});
